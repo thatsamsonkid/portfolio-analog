fe36b9def93855ea7fce17a6bb25682946d5c3df
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-06 10:11:50 -0500

    fix: style clean up

--- src/app/components/circle.component.ts
+++ src/app/components/circle.component.ts
@@ -5,7 +5,11 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	template: `
-		<div class="circle-display p-4 text-center {{ class }}">
+		<div
+			class="flex w-[125px] h-[125px] shadonw-lg shadow-shadow-black rounded-full justify-center items-center p-4 text-center flex-col {{
+				class
+			}}"
+		>
 			<span class="circle-display__title sm-subtitle-2 mb-3">
 				{{ title }}
 			</span>
