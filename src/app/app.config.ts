de7c3dd64fe0fd98eebd477a44d0976199df84ff
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-01-23 22:20:23 -0500

    deps upgrade + zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+	ApplicationConfig,
+	ɵprovideZonelessChangeDetection,
+} from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideFileRouter(),
-    provideHttpClient(withFetch()),
-    provideClientHydration(),
-  ],
+	providers: [
+		provideFileRouter(),
+		provideHttpClient(withFetch()),
+		provideClientHydration(),
+		ɵprovideZonelessChangeDetection(),
+	],
 };
